d6e40896842197fc2577c9427d635d8f0766cee8
Thomas Newman <thomblweed@gmail.com>
2020-10-31 21:08:07 +0000

    generics start for get all products with relational data

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IGenericRespository<Product> _productRepo;
+        private readonly IGenericRespository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRespository<ProductType> _productTypeRepo;
+
+        public ProductsController(
+            IGenericRespository<Product> productRepo,
+            IGenericRespository<ProductBrand> productBrandRepo,
+            IGenericRespository<ProductType> productTypeRepo
+        )
+        {
+            _productRepo = productRepo;
+            _productBrandRepo = productBrandRepo;
+            _productTypeRepo = productTypeRepo;
+        }
+
+        [HttpGet]
+        // Task wraps method in async call, bit like a Promise in node
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+
+            IReadOnlyList<Product> products = await _productRepo.ListAsync(spec);
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _productRepo.GetByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productBrandRepo.ListAllAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productTypeRepo.ListAllAsync());
+        }
+    }
+}
---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
