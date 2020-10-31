25c28aa3bc3904ce809c81c3dbea1963d10ec824
Thomas Newman <thomblweed@gmail.com>
2020-10-31 10:17:58 +0000

    initial commit

---
+++ API/Controllers/ProdcutsController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        // Task wraps method in async call, bit like a Promise in node
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
